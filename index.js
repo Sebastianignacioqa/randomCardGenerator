0f831aaba0438a1066b3c804e03190230920f80a
Sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-08-12 02:03:08 +0000

    Numero y palo sin ciclo

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ window.onload = () => {
 
 
 let misPalos = ["♦","♥","♠","♣"]
-let randomPalo = parseInt(Math.floor(Math.random() * 4));
+let randomPalo = Math.floor(Math.random() * 4);
 let randomNumber = Math.floor(Math.random() * 12);
 let palo = document.createElement("H2");
 
-for (let randomPalo in misPalos) {
-    palo.innerHTML = misPalos[randomPalo];
+palo.innerHTML = misPalos[randomPalo];
     
-}
+
 
 let numero = document.createElement("H1");
 numero.innerHTML = randomNumber;
