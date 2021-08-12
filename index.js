79051c81af84bfd4538c2d75daac70459f3feb36
Sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-08-12 01:53:22 +0000

    Agregado 1 palo a carta

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-import "style.css";
 
 window.onload = () => {
+
+
+let misPalos = ["♦","♥","♠","♣"]
+let randomPalo = parseInt(Math.floor(Math.random() * 4));
+let randomNumber = Math.floor(Math.random() * 12);
+let palo = document.createElement("H2");
+
+for (let randomPalo in misPalos) {
+    palo.innerHTML = misPalos[randomPalo];
+    
+}
+
+let numero = document.createElement("H1");
+numero.innerHTML = randomNumber;
+
+let carta = document.querySelector(".carta");
+    carta.appendChild(numero);
+    carta.appendChild(palo);
+
+
+    
+
+
+numero.innerHTML
 
 
 
