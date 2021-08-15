00af33bb98af048196b3930650926a1d6069168b
Sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-08-15 18:39:14 +0000

    Terminada base de carta aleatoria

--- index.js
+++ index.js
@@ -2,33 +2,45 @@
 window.onload = () => {
 
 
-let misPalos = ["♦","♥","♠","♣"]
+let misPalos = ["♦","♥","♠","♣"];
+let letras = ["A", "J","Q","K"];
 let randomPalo = Math.floor(Math.random() * 4);
-let randomNumber = Math.floor(Math.random() * 12);
-let palo = document.createElement("H2");
+let randomNumber = Math.floor(Math.random() * 14);
+let randomLetra = Math.floor(Math.random() * 4);
 
+let palo = document.createElement("H2");
 palo.innerHTML = misPalos[randomPalo];
+
+let palo2 = document.createElement("H3");
+palo2.innerHTML = misPalos[randomPalo]
     
 
 
 let numero = document.createElement("H1");
+
+if (randomNumber <=10) {
 numero.innerHTML = randomNumber;
+}
+else if (randomNumber>10) {
+numero.innerHTML = letras[randomLetra]
+}
 
 let carta = document.querySelector(".carta");
+    carta.appendChild(palo2);
     carta.appendChild(numero);
     carta.appendChild(palo);
-
-
     
-
+if (randomPalo === 0 ) {
+    palo.style.color = "red"
+    palo2.style.color = "red"
+    
+}
+else if (randomPalo === 1) {
+    palo.style.color = "red"
+    palo2.style.color = "red"
+}
 
 numero.innerHTML
-
-
-
-
-
-
 
 
 }
